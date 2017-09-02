1b885c1d0df1000a3347cfdd706d6f25295e5aeb
Renato Dehnhardt <renatotkd@gmail.com>
2017-09-02 18:05:58 -0300

    Removendo arquivos da master

--- autoload.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-try {
-    (new Dotenv\Dotenv(__DIR__))->load();
-} catch (Dotenv\Exception\InvalidPathException $e) {
-}
--- public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../autoload.php';
-
-$app = new App\Application();
-$app->run();
